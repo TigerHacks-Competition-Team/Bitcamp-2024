352164f8fc69d2d0f303e6e80243d062e842b64b
Anh Pham <demonxranger8@gmail.com>
2024-04-20 12:42:49 -0400

    add placeholder icon

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,47 +1,52 @@
 <script lang="ts">
-	import Water from "$lib/components/water/Water.svelte";
-	import CardContent from "$lib/components/ui/card/card-content.svelte";
-	import CardDescription from "$lib/components/ui/card/card-description.svelte";
-	import CardFooter from "$lib/components/ui/card/card-footer.svelte";
-	import CardHeader from "$lib/components/ui/card/card-header.svelte";
-	import CardTitle from "$lib/components/ui/card/card-title.svelte";
-	import Card from "$lib/components/ui/card/card.svelte";
-	import { onMount } from "svelte";
-    import { app, auth, poolsRef, store, user, usersRef } from "$lib/api/firebase"
-    import { collection, getDoc, setDoc, addDoc, updateDoc, getDocs, QueryDocumentSnapshot, where, query, doc } from 'firebase/firestore';
+	import Water from '$lib/components/water/Water.svelte';
+	import CardContent from '$lib/components/ui/card/card-content.svelte';
+	import CardDescription from '$lib/components/ui/card/card-description.svelte';
+	import CardHeader from '$lib/components/ui/card/card-header.svelte';
+	import CardTitle from '$lib/components/ui/card/card-title.svelte';
+	import Card from '$lib/components/ui/card/card.svelte';
+	import { poolsRef, user, usersRef } from '$lib/api/firebase';
+	import {
+		getDoc,
+		doc
+	} from 'firebase/firestore';
 
-	let shake: (amount: number) => void;
+	const getData = async () => {
+		const userSnapshot = await getDoc(doc(usersRef, $user?.uid));
+		const pools = userSnapshot?.data()?.pools;
+		const outPools = [];
 
-    const getData = async () => {
-        const userSnapshot = await getDoc(doc(usersRef, $user?.uid));
-        const pools = userSnapshot?.data()?.pools
-        const outPools = [];
+		if (!pools) {
+			console.warn('Failed to get user pools!', userSnapshot);
+			return;
+		}
 
-        if (!pools) { console.warn("Failed to get user pools!", userSnapshot); return; }
+		for (const pool of pools) {
+			const poolSnapshot = await getDoc(doc(poolsRef, pool));
+			if (!poolSnapshot.exists) {
+				console.warn('Tried to get invalid pool!');
+				continue;
+			}
 
-        for (const pool of pools) {
-            const poolSnapshot = await getDoc(doc(poolsRef, pool));
-            if (!poolSnapshot.exists) { console.warn("Tried to get invalid pool!"); continue; }
+			outPools.push(poolSnapshot);
+		}
 
-            outPools.push(poolSnapshot);
-        }
-        
-        return outPools;
-    }
+		return outPools;
+	};
 </script>
 
 {#await getData()}
-    ...
+	...
 {:then data}
-    {#each data as pool}
-        <Card class="flex justify-between h-28">
-            <CardHeader class="w-1/2">
-                <CardTitle>{JSON.stringify(pool.data().name)}</CardTitle>
-                <CardDescription>Ammount Due: {pool.due}</CardDescription>
-            </CardHeader>
-            <CardContent class="p-0 h-4/5 max-w-full aspect-square mr-4 self-center">
-                <Water></Water>
-            </CardContent>
-        </Card>
-    {/each}
+	{#each data as pool}
+		<Card class="flex justify-between h-28">
+			<CardHeader class="w-1/2">
+				<CardTitle>{JSON.stringify(pool.data().name)}</CardTitle>
+				<CardDescription>Amount Due: {pool.due}</CardDescription>
+			</CardHeader>
+			<CardContent class="p-0 h-4/5 max-w-full aspect-square mr-4 self-center">
+				<Water></Water>
+			</CardContent>
+		</Card>
+	{/each}
 {/await}
