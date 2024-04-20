b0bad1facdf41f551207e88bf4b58a9771beae78
TesDevelopment <joshuaepee1@gmail.com>
2024-04-20 02:48:35 -0400

    Adapter / Tcsonfig Changes

---
+++ src/routes/+server.ts
@@ -0,0 +1,7 @@
+export async function GET({ url: any }) {
+
+    const response = await fetch('https://example.com');
+
+    console.log("Api call made to https://example.com")
+    return new Response(await response.text());
+}
