01cfe7b8581f83616475a555df4da64b7b01e540
Anh Pham <demonxranger8@gmail.com>
2024-04-20 03:12:05 -0400

    sign up

---
+++ src/lib/api/firebase.ts
@@ -0,0 +1,34 @@
+import { getApps, initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import {
+	getAuth,
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+    type User,
+} from 'firebase/auth';
+
+import { writable } from 'svelte/store';
+
+const firebaseConfig = {
+	apiKey: import.meta.env.VITE_FB_KEY,
+	authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_FB_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FB_SENDER_ID,
+	appId: import.meta.env.VITE_FB_APP_ID,
+	measurementId: import.meta.env.VITE_FB_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+export const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
+//export const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const user = writable<User | null>(null);
+
+export const signUpEmailAndPassword = (email: string, password: string) => {
+	return createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInEmailAndPassword = (email: string, password: string) => {
+	return signInWithEmailAndPassword(auth, email, password);
+};
---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { onAuthStateChanged } from "firebase/auth";
+    import { onMount } from "svelte";
+    import { auth, user } from "$lib/api/firebase";
+	import SignUp from "./SignUp.svelte";
+
+    let screen = "sign-in";
+
+    onMount(() => {
+        onAuthStateChanged(auth, (u) => {
+            $user = u;
+        })
+    });
+</script>
+
+<div>
+    {#if screen == "sign-in"}
+        <SignUp></SignUp>
+    {:else}
+        <div>
+
+        </div>
+    {/if}
+</div>
