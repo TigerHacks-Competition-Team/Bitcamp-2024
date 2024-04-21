ea1df369b6e0d4125293ab08332b2f914f7692af
Gus Ruben <gusruben@protonmail.com>
2024-04-21 06:07:17 -0400

    Update sign in page with link and gradient

--- src/routes/auth/sign-in/+page.svelte
+++ src/routes/auth/sign-in/+page.svelte
@@ -1,40 +1,48 @@
 <script lang="ts">
-	import Input from '$lib/components/ui/input/input.svelte';
-	import { signInEmailAndPassword } from '$lib/api/firebase';
-	import { Label } from '$lib/components/ui/label';
-	import { Button } from '$lib/components/ui/button';
-	import { toast } from 'svelte-sonner';
-	import { goto } from '$app/navigation';
-	import LoginIcon from '$lib/components/icons/LoginIcon.svelte';
-	import { auth } from '$lib/api/firebase';
-	import Separator from '$lib/components/ui/separator/separator.svelte';
+	import Input from "$lib/components/ui/input/input.svelte";
+	import { signInEmailAndPassword } from "$lib/api/firebase";
+	import { Label } from "$lib/components/ui/label";
+	import { Button } from "$lib/components/ui/button";
+	import { toast } from "svelte-sonner";
+	import { goto } from "$app/navigation";
+	import LoginIcon from "$lib/components/icons/LoginIcon.svelte";
+	import { auth } from "$lib/api/firebase";
+	import Separator from "$lib/components/ui/separator/separator.svelte";
 
-    let email = "";
-    let password = "";
+	let email = "";
+	let password = "";
 
-    const onSubmit = () => {
-        signInEmailAndPassword(email, password).catch(e => {
-			toast(`${e.name}: ${e.code}`);
-		}).then(() => {
-
-			if(auth.currentUser)
-				goto("/home");
-		});
-    }
+	const onSubmit = () => {
+		signInEmailAndPassword(email, password)
+			.catch(e => {
+				toast(`${e.name}: ${e.code}`);
+			})
+			.then(() => {
+				if (auth.currentUser) goto("/home");
+			});
+	};
 </script>
 
 <div>
+	<div
+		class="fixed inset-0"
+		style="background: radial-gradient(57.01% 13.63% at 50% 0%, #111E2D 0%, #17181D 100%), #17181D"
+	></div>
 	<form on:submit|preventDefault={onSubmit}>
 		<div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-2 w-[80vw]">
 			<h1 class="text-3xl text-center mb-2">Log In</h1>
-			<Input placeholder="Email" bind:value={email}/>
-			<Input placeholder="Password" type="password" class="mb-2" bind:value={password}/>
+			<Input placeholder="Email" bind:value={email} />
+			<Input placeholder="Password" type="password" class="mb-2" bind:value={password} />
 			<Button variant="fancy" type="submit">
-				<LoginIcon size={20} color="#66A3EB" class="mr-1"/>
+				<LoginIcon size={20} color="#66A3EB" class="mr-1" />
 				Sign In
 			</Button>
 			<Separator />
-			<p class="text-muted-foreground text-xs text-center">Need an account? <a href="/auth/sign-in" class="cursor-pointer text-primary-foreground hover:underline">Sign up</a> instead.</p>
+			<p class="text-muted-foreground text-xs text-center">
+				Need an account? <a href="/auth/sign-up" class="cursor-pointer text-primary-foreground hover:underline"
+					>Sign up</a
+				> instead.
+			</p>
 		</div>
 		<!-- <div>
 			<div>
