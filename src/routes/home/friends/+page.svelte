b20f24ffc9b171c2887c0377e7c48d4690cb0d02
Anh Pham <demonxranger8@gmail.com>
2024-04-21 07:04:36 -0400

    add friend

--- src/routes/home/friends/+page.svelte
+++ src/routes/home/friends/+page.svelte
@@ -4,8 +4,13 @@
 	import { Button, buttonVariants } from "$lib/components/ui/button";
 	import * as Dialog from "$lib/components/ui/dialog";
 	import { Input } from "$lib/components/ui/input";
+	import ScrollArea from "$lib/components/ui/scroll-area/scroll-area.svelte";
+	import { onMount } from "svelte";
 	import FriendCard from "./FriendCard.svelte";
 	import { toast } from "svelte-sonner";
+
+	let friends: User[];
+	let friendRequests: User[];
 
 	const getFriends = async () => {
 		if (!browser) return;
@@ -37,22 +42,37 @@
 			//res.user.friends[i].due = 0;
 		}
 
-		return res.user.friends;
+		for (const i in res.user.friend_requests) {
+			const requesterId: string = res.user.friend_requests[i];
+
+			const user = await (
+				await fetch("/api/v1/user/" + requesterId, {
+					headers: {
+						auth_token: u.uid,
+					},
+				})
+			).json();
+
+			res.user.friend_requests[i] = user.user;
+		}
+
+		friends = res.user.friends;
+		friendRequests = res.user.friend_requests;
 	};
 
-    let emailToAdd = "";
-    let open = false;
+	let emailToAdd = "";
+	let open = false;
 
-    const addFriend = async () => {
-		const friendId = await(
+	const sendRequest = async () => {
+		const friendId = await (
 			await fetch(`/api/v1/user/get_id_from_email`, {
 				method: "POST",
 				headers: {
 					auth_token: $user!.uid,
 				},
 				body: JSON.stringify({
-					email: emailToAdd
-				})
+					email: emailToAdd,
+				}),
 			})
 		).json();
 
@@ -60,46 +80,84 @@
 			toast.error("Cannot find user");
 		}
 
-        fetch(`/api/v1/user/${$user!.uid}/friend`, {
-            method: "POST",
-            headers: {
-                auth_token: $user!.uid
-            },
-            body: JSON.stringify({
-                friend_id: friendId["id"]
-            })
-        })
+		fetch(`/api/v1/user/${$user!.uid}/friend`, {
+			method: "POST",
+			headers: {
+				auth_token: $user!.uid,
+			},
+			body: JSON.stringify({
+				friend_id: friendId["id"],
+			}),
+		});
 
-		toast.success("Friend added!");
+		toast.success("Friend request sent!");
 
-        open = false;
-    }
+		open = false;
+	};
+
+	const addFriend = (id: string) => {
+		fetch(`/api/v1/user/${$user!.uid}/accept_request`, {
+			method: "POST",
+			headers: {
+				auth_token: id,
+			}
+		});
+	}
+
+	onMount(() => {
+		getFriends();
+	})
 </script>
 
-<div class="flex flex-col gap-4">
-	<h1 class="text-xl">Friends</h1>
+<ScrollArea>
+	<div class="flex flex-col gap-4">
+		<h1 class="text-xl">Friends</h1>
 
-	<Dialog.Root bind:open>
-		<Dialog.Trigger class={buttonVariants({ variant: "fancy" })}>Add a Friend</Dialog.Trigger>
+		<Dialog.Root bind:open>
+			<Dialog.Trigger class={buttonVariants({ variant: "fancy" })}>Add a Friend</Dialog.Trigger>
 
-		<Dialog.Content class="w-[90%]">
-			<Dialog.Header>
-				<Dialog.Title>Add with Email</Dialog.Title>
-			</Dialog.Header>
+			<Dialog.Content class="w-[90%]">
+				<Dialog.Header>
+					<Dialog.Title>Add with Email</Dialog.Title>
+				</Dialog.Header>
 
-            <Input id="email" autocomplete="off" placeholder="john@appleseed.com" bind:value={emailToAdd}/>
+				<Input id="email" autocomplete="off" placeholder="john@appleseed.com" bind:value={emailToAdd} />
 
-			<Dialog.Footer>
-				<Button variant="fancy" on:click={addFriend}>Add</Button>
-			</Dialog.Footer>
-		</Dialog.Content>
-	</Dialog.Root>
+				<Dialog.Footer>
+					<Button variant="fancy" on:click={sendRequest}>Add</Button>
+				</Dialog.Footer>
+			</Dialog.Content>
+		</Dialog.Root>
 
-	{#await getFriends()}
-		...
-	{:then friends}
-		{#each friends as friend}
-			<FriendCard {friend} />
-		{/each}
-	{/await}
-</div>
+		{#if friends}
+			{#each friends as friend}
+				<a href={`/home/friends/${friend.id}`}>
+					<FriendCard {friend} />
+				</a>
+				
+			{/each}
+		{/if}
+
+		<h1 class="text-xl">Friend Requests</h1>
+
+		{#if friendRequests}
+			{#each friendRequests as request}
+				<Dialog.Root>
+					<Dialog.Trigger class={buttonVariants({ variant: "outline" })}>
+						<FriendCard {request} />
+					</Dialog.Trigger>
+
+					<Dialog.Header>
+						<Dialog.Title>Add friend?</Dialog.Title>
+					</Dialog.Header>
+
+					<Dialog.Footer>
+						<Button variant="fancy" on:click={() => addFriend(request.id)}>Add</Button>
+						<Button variant="fancy">Cancel</Button>
+					</Dialog.Footer>
+				</Dialog.Root>
+				
+			{/each}
+		{/if}
+	</div>
+</ScrollArea>
