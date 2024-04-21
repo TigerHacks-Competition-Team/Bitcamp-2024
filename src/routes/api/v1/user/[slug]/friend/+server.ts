ae58e489443c5b5edac737c98769b5341612569c
RuadhanKM <9281239@students.k12.dc.us>
2024-04-21 08:55:30 -0400

    friend requests

--- src/routes/api/v1/user/[slug]/friend/+server.ts
+++ src/routes/api/v1/user/[slug]/friend/+server.ts
@@ -15,13 +15,18 @@ export async function POST(event: RequestEvent) {
     const user = event.locals.user;
 
     const friend = (await getDoc(doc(store, `users/${req.friend_id}`))).data();
+    const me = (await getDoc(doc(store, `users/${auth_token}`))).data();
     
     if (friend!.friend_requests.includes(user.id) || friend!.friends.includes(user.id))
         return new Response(stringify({ passed: false, error: "Friend request already sent" }), { status: 400 });
+    if (me!.friend_requests.includes(friend.id) || me!.friends.includes(friend.id))
+        return new Response(stringify({ passed: false, error: "Friend request already sent" }), { status: 400 });
     
     friend!.friend_requests.push(user.id);
+    me!.friend_requests.push(friend.id);
 
     await setDoc(doc(store, `users/${req.friend_id}`), friend);
+    await setDoc(doc(store, `users/${auth_token}`), me);
 
     return new Response(stringify({ passed: true }), { status: 200 });
 }
